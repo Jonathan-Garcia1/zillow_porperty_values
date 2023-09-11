5a41e015841632ba23b7b809f9418832df9a3172
Jonathan Garcia <jonathan.garcia.0136@gmail.com>
2023-09-11 12:31:53 -0500

    Zillow Report_plus update

--- explore_plus.py
+++ explore_plus.py
@@ -200,8 +200,121 @@ def MinMax_Scaler(df):
     
     mms = MinMaxScaler()
 
-    to_scale = ['bedrooms', 'bathrooms', 'area', 'year']
+    to_scale = df.select_dtypes(include=['float', 'int']).columns.tolist()
 
     df[to_scale] = mms.fit_transform(df[to_scale])
+    
+    return df
+
+def hot_encode(df):
+    # Use pd.get_dummies to one-hot encode the DataFrame
+    df = pd.get_dummies(df, drop_first=False)
+    
+    return df
+
+
+def simple_features(df):
+    """
+    Create custom features for the given DataFrame.
+
+    This function adds the following features to the DataFrame:
+    - 'total_rooms': Total number of rooms, calculated as the sum of bedrooms and bathrooms.
+    - 'age': Age of each entry, calculated as the difference between the current year and the 'year' column.
+    - 'bdrm_area_ratio': Bedroom-to-area ratio, calculated as 'area' divided by 'bedrooms'.
+    - 'bath_bdrm_ratio': Bathroom-to-bedroom ratio, calculated as 'bathrooms' divided by 'bedrooms'.
+
+    Parameters:
+    df (pd.DataFrame): The original DataFrame containing the necessary columns.
+
+    Returns:
+    pd.DataFrame: A DataFrame with the additional features added.
+    """
+
+    # Calculate total_rooms by summing bedrooms and bathrooms
+    df['total_rooms'] = df['bedrooms'] + df['bathrooms']
+
+    # Get the current year
+    current_year = datetime.datetime.now().year
+
+    # Calculate the age of each entry based on the 'year' column
+    df['age'] = current_year - df['year']
+
+    # Calculate the bedroom-to-area ratio
+    df['bdrm_area_ratio'] = df['area'] / df['bedrooms']
+
+    # Calculate the bathroom-to-bedroom ratio
+    df['bath_bdrm_ratio'] = df['bathrooms'] / df['bedrooms']
+
+    return df
+
+
+def age_group(df):
+    """
+    Create an 'age_group' column in the DataFrame based on age categories.
+
+    This function adds an 'age_group' column to the DataFrame, categorizing entries based on their 'age' values
+    into predefined age groups.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame containing the 'age' column.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the 'age_group' column added.
+    """
+
+    # Define the age group categories and labels
+    age_bins = [-1, 4, 9, 19, 39, 59, 79, 200]
+    age_labels = ['newest', 'very_new', 'new', 'mid', 'old', 'very_old', 'oldest']
+
+    # Create the 'age_group' column based on the age bins and labels
+    df['age_group'] = pd.cut(df['age'], bins=age_bins, labels=age_labels)
+
+    return df
+
+
+def size_group(df):
+    """
+    Create a 'size_group' column in the DataFrame based on area percentiles.
+
+    This function adds a 'size_group' column to the DataFrame, categorizing entries based on their 'area' values
+    into predefined size groups defined by percentiles.
+
+    Parameters:
+    df (pd.DataFrame): The DataFrame containing the 'area' column.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the 'size_group' column added.
+    """
+    # Calculate the percentiles
+    percentiles = df['area'].quantile([0.01, 0.05, 0.32, 0.68, 0.95, 0.99])
+
+    # Define the categories
+    df['size_group'] = pd.cut(
+        df['area'],
+        bins=[0, percentiles[0.01], percentiles[0.05], percentiles[0.32], percentiles[0.68], percentiles[0.95], percentiles[0.99], df['area'].max()],
+        labels=['smallest', 'very_small', 'small', 'mid', 'large', 'very_large', 'largest']
+    )
+    
+    return df
+
+
+def features(df):
+    """
+    Create and add multiple features to the DataFrame.
+
+    This function applies three separate feature creation functions to the input DataFrame:
+    - simple_features: Adds basic features like 'total_rooms', 'age', 'bdrm_area_ratio', and 'bath_bdrm_ratio'.
+    - age_group: Adds an 'age_group' column based on predefined age categories.
+    - size_group: Adds a 'size_group' column based on area percentiles.
+
+    Parameters:
+    df (pd.DataFrame): The original DataFrame.
+
+    Returns:
+    pd.DataFrame: The DataFrame with the added features.
+    """
+    df = simple_features(df)
+    df = age_group(df)
+    df = size_group(df)
     
     return df
